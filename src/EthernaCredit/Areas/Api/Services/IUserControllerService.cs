1c99f1d97e091bf49fb6a039a6095b48b0f8bbaa
Mirko Da Corte <mirko@etherna.io>
2020-09-02 01:09:52 +0200

    Implemented user api

---
+++ src/EthernaCredit/Areas/Api/Services/IUserControllerService.cs
@@ -0,0 +1,12 @@
+﻿using Etherna.EthernaCredit.Areas.Api.DtoModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Etherna.EthernaCredit.Areas.Api.Services
+{
+    public interface IUserControllerService
+    {
+        Task<IEnumerable<LogDto>> GetLogsAsync(int page, int take);
+        Task<double> GetCreditAsync();
+    }
+}
