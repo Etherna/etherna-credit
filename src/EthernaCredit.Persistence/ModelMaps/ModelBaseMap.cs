0fd6634c2ff80de0400fc8e850b09831f77157ee
Mirko Da Corte <mirko@etherna.io>
2021-11-15 19:35:09 +0100

    Added EthernaRCL Added status page Fixed issues

--- src/EthernaCredit.Persistence/ModelMaps/ModelBaseMap.cs
+++ src/EthernaCredit.Persistence/ModelMaps/ModelBaseMap.cs
@@ -13,7 +13,7 @@ namespace Etherna.CreditSystem.Persistence.ModelMaps
         {
             // register class maps.
             dbContext.SchemaRegister.AddModelMapsSchema<ModelBase>("92a5e4a2-6b0c-46b7-aac7-70380deda7b4");
-
+            dbContext.SchemaRegister.AddModelMapsSchema<EntityModelBase>("155af6aa-92d1-4d8b-b000-5b5464f84dc7");
             dbContext.SchemaRegister.AddModelMapsSchema<EntityModelBase<string>>("a0fddc7f-b64d-43b0-a40d-389b3fb21b67",
                 mm =>
                 {
