7baa595c4feb2023a0a63b37e534e928392de3e9
Mirko Da Corte <mirko@etherna.io>
2022-01-18 20:00:01 +0100

    Implemented daily comulative balance update log

--- src/EthernaCredit.Persistence/ModelMaps/Credit/OperationLogMap.cs
+++ src/EthernaCredit.Persistence/ModelMaps/Credit/OperationLogMap.cs
@@ -1,5 +1,7 @@
 ﻿using Etherna.CreditSystem.Domain.Models;
 using Etherna.CreditSystem.Domain.Models.OperationLogs;
+using Etherna.MongoDB.Bson;
+using Etherna.MongoDB.Bson.Serialization.Serializers;
 using Etherna.MongODM.Core;
 using Etherna.MongODM.Core.Extensions;
 using Etherna.MongODM.Core.Serialization;
@@ -16,13 +18,11 @@ namespace Etherna.CreditSystem.Persistence.ModelMaps.Credit
                     mm.AutoMap();
 
                     // Set members with custom serializers.
+                    mm.SetMemberSerializer(l => l.Amount, new Decimal128Serializer(BsonType.Decimal128));
                     mm.SetMemberSerializer(l => l.User, UserMap.ReferenceSerializer(dbContext));
                 });
-
             dbContext.SchemaRegistry.AddModelMapsSchema<DepositOperationLog>("7fc7abe8-9a55-40a2-90ce-f3cba34bc005");
-
             dbContext.SchemaRegistry.AddModelMapsSchema<UpdateOperationLog>("74e021d4-6d86-4deb-b952-0c328839cfe2");
-
             dbContext.SchemaRegistry.AddModelMapsSchema<WithdrawOperationLog>("b0ffe059-c985-4f3d-8677-238ab9551ec3");
         }
     }
