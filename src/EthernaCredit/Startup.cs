617ffce703c2273764e8b0a36775bf4877037484
Mirko Da Corte <mirko@etherna.io>
2022-01-21 00:15:51 +0100

    enabled middleware

--- src/EthernaCredit/Startup.cs
+++ src/EthernaCredit/Startup.cs
@@ -40,6 +40,7 @@ using Microsoft.AspNetCore.Authorization.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -82,6 +83,11 @@ namespace Etherna.CreditSystem
                     ConnectionString = Configuration["ConnectionStrings:DataProtectionDb"] ?? throw new ServiceConfigurationException()
                 })
                 .SetApplicationName(CommonConsts.SharedCookieApplicationName);
+
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardedHeaders = ForwardedHeaders.All;
+            });
 
             services.AddCors();
             services.AddRazorPages(options =>
@@ -279,10 +285,12 @@ namespace Etherna.CreditSystem
             if (Environment.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseForwardedHeaders();
             }
             else
             {
                 app.UseExceptionHandler("/Error");
+                app.UseForwardedHeaders();
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
