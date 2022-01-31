6d9d0658433bf1893109eaa0498ae099ad0660c9
Mattia Dalzocchio <mattiadalzocchio@me.com>
2022-01-31 14:55:46 +0100

    Enable CORS from any origin

--- src/EthernaCredit/Startup.cs
+++ src/EthernaCredit/Startup.cs
@@ -317,21 +317,10 @@ namespace Etherna.CreditSystem
 
             app.UseCors(builder =>
             {
-                if (Environment.IsDevelopment())
-                {
-                    builder.SetIsOriginAllowed(_ => true)
-                           .AllowAnyHeader()
-                           .AllowAnyMethod()
-                           .AllowCredentials();
-                }
-                else
-                {
-                    builder.WithOrigins("https://*.etherna.io")
-                           .SetIsOriginAllowedToAllowWildcardSubdomains()
-                           .AllowAnyHeader()
-                           .AllowAnyMethod()
-                           .AllowCredentials();
-                }
+                builder.SetIsOriginAllowed(_ => true)
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowCredentials();
             });
 
             app.UseHttpsRedirection();
