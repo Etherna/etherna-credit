7f9aa8f826f3f1430a9efb2fc19843b029832cfd
Mirko Da Corte <mirko@etherna.io>
2023-10-16 18:49:30 +0200

    fix serilog config

--- src/EthernaCredit/Program.cs
+++ src/EthernaCredit/Program.cs
@@ -72,6 +72,7 @@ namespace Etherna.CreditSystem
             var configuration = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{environment}.json", optional: true)
+                .AddEnvironmentVariables()
                 .Build();
 
             Log.Logger = new LoggerConfiguration()
